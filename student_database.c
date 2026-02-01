c2a598f62eef0b60d243e7e9b13f9aa58af7302c
John Wolbert <jtwolbert52@gmail.com>
2026-02-01 10:33:58 -0500

    add warehouse system/rm student database

--- student_database.c
+++ student_database.c
@@ -1,51 +1,38 @@
 #include <stdio.h>
-#include <string.h>
 
-struct Student{
-        char name[50];
-        char id[9];
-        float gpa;
+struct Dimensions {
+            float length;
+            float width;
+            float height;
+        };
+
+struct Box {
+        char label[50];
+        float weight;
+        struct Dimensions size;
     };
+
+float calcVolume(struct Box b){
+    return b.size.length * b.size.width * b.size.height;
+}
+
+float calcDensity(struct Box b, float vol){
+    return b.weight / vol;
+}
 
 int main(){
     
-    //struct Student s1;
-    //struct Student s2;
-
-    // strcpy(s1.name, "John");
-    // strcpy(s1.id, "01515805");
-    // s1.gpa = 4.0;
-
-    // strcpy(s2.name, "Matt");
-    // strcpy(s2.id, "04891565");
-    // s2.gpa = 1.9;
-
-    struct Student s1 = {
-        "John",
-        "R01515805",
-        4.0
+    struct Box b1 = {
+        "Box of Rocks",
+        30.0,
+        {9.1, 12.45, 12.5231}
     };
 
-    struct Student s2 = {
-        "Matt", 
-        "R04891565", 
-        1.9
-    };
+    float vol1 = calcVolume(b1);
 
-    printf("Name: %s\n", s1.name);
-    printf("ID: %s\n", s1.id);
-    printf("GPA: %.1f\n\n", s1.gpa);
-
-    printf("Name: %s\n", s2.name);
-    printf("ID: %s\n", s2.id);
-    printf("GPA: %.1f\n\n", s2.gpa);
-
-    if (s1.gpa < 2.0){
-        printf("%s needs to improve their gpa.\n", s1.name);
-    }
-    if (s2.gpa < 2.0){
-        printf("%s needs to improve their gpa.\n", s2.name);
-    }
+    printf("Volume: %.3f ft^3\n", vol1);
+    printf("Density: %.3f g/cm^3\n", calcDensity(b1, vol1));
 
     return 0;
 }
+
